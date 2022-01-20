6a2c7746e05de5d9d92012dd0b31a6d3679e1a17
GoatedChopin <85979611+GoatedChopin@users.noreply.github.com>
2022-01-20 15:04:42 -0700

    v 0.2

--- parse_patient_csv.py
+++ parse_patient_csv.py
@@ -4,7 +4,7 @@ import numpy as np
 patient_csv = "Data_Entry_2017.csv"
 patient_data = pd.read_csv(patient_csv)
 
-conditions = {Atelectasis:0, Consolidation:1, Infiltration:2, Pneumothorax:3, Edema:4, Emphysema:5, Fibrosis:6, Effusion:7, Pneumonia:8, Pleural_thickening:9, Cardiomegaly:10, Nodule:11, Mass:12, Hernia:13}
+conditions = {"Atelectasis":0, "Consolidation":1, "Infiltration":2, "Pneumothorax":3, "Edema":4, "Emphysema":5, "Fibrosis":6, "Effusion":7, "Pneumonia":8, "Pleural_Thickening":9, "Cardiomegaly":10, "Nodule":11, "Mass":12, "Hernia":13}
 
 def split_finding(finding):
   split = finding.split(r"|")
@@ -15,10 +15,12 @@ def split_finding(finding):
   return(cond_list)
 
 if __name__ == "__main__":
-  pd.get_dummies(data = patient_data, columns = ["Patient Gender", "View Position"])
+  dummies = pd.get_dummies(data = patient_data, columns = ["Patient Gender", "View Position"]).columns
   # What happens after the get_dummies? I need to select the binary variables in the following section, rather than the original ones.
   
-  columns = ["Image Index", "Patient Age", "Patient Gender", "View Position"]
-  subset_data = patient_data[columns]
+  dummy_columns = ["Patient Gender_M", "View Position_PA"]
+  subset_data = patient_data[["Image Index", "Patient Age"]]
+  subset_data["Male"], uniques = pd.factorize(patient_data["Patient Gender"])
+  subset_data["PA"], uniques = pd.factorize(patient_data["View Position"])
   subset_data["conditions"] = [split_finding(i) for i in patient_data["Finding Labels"]]
-  pd.to_csv(r"\relevant_patient_data.csv")
+  subset_data.to_csv("subset.csv", index = False)
