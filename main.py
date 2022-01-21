1508f37816d9b9cb1ee96d992a192679c430c695
GoatedChopin <85979611+GoatedChopin@users.noreply.github.com>
2022-01-21 15:01:02 -0700

    Minor Optimizations

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 subset = pd.read_csv("subset.csv")
 
+# Change images_path as needed
 list_of_files = []
 images_path = "Images"
 for (dirpath, dirnames, filenames) in os.walk(images_path):
@@ -18,10 +19,8 @@ conditions_list = []
 for image_path in list_of_files:
     image = Image.open(image_path)
     conditions = np.asarray(subset.loc[subset["Image_Index"]==filename]["Conditions"].to_list()[0][1:-1].split(".")[0:14]).astype('float32')
-    image_conditions_pair = {"image":np.asarray(image), "conditions":conditions}
-    
-    image_list.append(image_conditions_pair["image"])
-    conditions_list.append(image_conditions_pair["conditions"])
+    image_list.append(np.asarray(image))
+    conditions_list.append(conditions)
     
 image_array = np.asarray(image_list)
 conditions_array = np.asarray(conditions_list)
